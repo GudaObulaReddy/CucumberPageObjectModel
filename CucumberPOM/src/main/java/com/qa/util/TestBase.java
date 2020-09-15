0ae6836bae2e43d40733947cd56674cc66bb05a1
Ramana Reddy <Ramana Reddy@DESKTOP-B96A4O5>
2020-09-15 13:08:28 +0530

    uploading all the files

---
+++ CucumberPOM/src/main/java/com/qa/util/TestBase.java
@@ -0,0 +1,49 @@
+package com.qa.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase
+{
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public TestBase(){
+		
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream("C:\\Users\\Ramana Reddy\\Desktop\\selinium\\CucumberPOM\\src\\main\\java\\com\\qa\\config\\config.properties");
+			prop.load(ip);
+		} catch (IOException e) {
+			e.getMessage();
+		} 
+      }
+	
+	
+	public static void initialization(){
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ramana Reddy\\Desktop\\selinium\\CucumberPOM\\chromedriver.exe");	
+			driver = new ChromeDriver(); 
+		}
+		else if(browserName.equals("FF")){
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Ramana Reddy\\Desktop\\selinium\\CucumberPOM\\geckodriver.exe");	
+			driver = new FirefoxDriver(); 
+		}
+		
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+		
+}}
